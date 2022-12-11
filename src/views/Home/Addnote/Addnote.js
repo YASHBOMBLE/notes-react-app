3bb5e35bfa6b7a5f4df87b063f6fa709bd1e4378
yashbomble <yashbomble2002@gmail.com>
2022-12-11 13:58:24 +0530

    bootstrap added

---
+++ src/views/Home/Addnote/Addnote.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Addnote() {
+  return (
+    <div>
+      <h1>Add note</h1>
+    </div>
+  )
+}
+
+export default Addnote
---
+++ src/views/Home/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Home.css";
+function Home() {
+
+  return (
+    <>
+    <div className="row">
+        <div className='col-md-12'>
+          <div className='show-notes-container'>
+            <div className='all-notes-container'>
+              <div className='note'>
+              
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+    )
+}
+
+export default Home
