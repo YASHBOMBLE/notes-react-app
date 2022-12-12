19d41287109e0a1599d5f991ae7a8f1a4adb665f
yashbomble <yashbomble2002@gmail.com>
2022-12-12 16:12:23 +0530

    feat:Delete

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,11 +1,30 @@
-import React from 'react'
 
-function Note({title, content}) {
+import React from 'react'
+import swal from 'sweetalert'
+import "./Note.css"
+function Note({title, content,noteIndex}) {
+  function deleteNote(){
+    
+    const notes=localStorage.getItem("notes")
+    if(notes)
+    {
+      const temp=JSON.parse(notes);
+      temp.splice(noteIndex,1)
+      localStorage.setItem("notes",JSON.stringify(temp));
+    }
+    swal({
+      title:"Deleted",
+      Text : "Your Note has been deleted.",
+      icon:"Success"
+    })
+    window.location.reload();
+  }
   return (
     <div>
       <div className='note-container' >
-        <h2>{title}</h2>
+        <h6>{title}</h6>
         <p>{content}</p>
+        <span className='delete-btn' onClick={deleteNote}>❌</span>
       </div>
     </div>
   )
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -8,40 +8,51 @@ import Note from "../../components/Note/Note";
 function Home() {
     const [notes, setNotes] = useState([
         {
-            title : "Title 1",
-            content : "content 1"
+            title: "Title 1",
+            content: "content 1"
         }
     ])
-    const [title,setTitle]=useState("")
-    const [content,setContent]=useState("")
-    function addNote(){
-const newNote ={
-    title:title,
-    content:content
-}
-     //const temp=notes;
-     //temp.push(newNote)
-     //setNotes(temp)
-     if(title==="" || content==="")
-     {
-        swal({
-            title:"Error",
-            text:"please fill all field",
-            icon:"error"
-        })
-        return;
-     }
-     else{
 
-     }
-     setNotes([...notes,newNote])
+    useEffect(() => {
+        const notes = localStorage.getItem("notes")
+        if (notes) {
+            setNotes(JSON.parse(notes))
+        }
+    }, [])
+    useEffect(() => {
+        if (notes.length > 1) {
+            localStorage.setItem("notes", JSON.stringify(notes))
+        }
+    }, [notes])
+    const [title, setTitle] = useState("")
+    const [content, setContent] = useState("")
+    function addNote() {
+        const newNote = {
+            title: title,
+            content: content
+        }
+        //const temp=notes;
+        //temp.push(newNote)
+        //setNotes(temp)
+        if (title === "" || content === "") {
+            swal({
+                title: "Error",
+                text: "please fill all field",
+                icon: "error"
+            })
+            return;
+        }
+        else {
+
+        }
+        setNotes([...notes, newNote])
         swal({
-        title:"Note added",
-        text:"Notes added successfully",
-        icon:"success"
-    })
-     setTitle("")
-     setContent("")
+            title: "Note added",
+            text: "Notes added successfully",
+            icon: "success"
+        })
+        setTitle("")
+        setContent("")
     }
     return (
         <>
@@ -56,7 +67,7 @@ const newNote ={
                             <div className='note'>
                                 {notes.map((note, index) => {
                                     return (
-                                        <Note title={note.title} content={note.content} />
+                                        <Note title={note.title} content={note.content} noteIndex={index} />
                                     )
                                 })}
                             </div>
@@ -65,22 +76,22 @@ const newNote ={
                     </div>
                 </div>
                 <div className='col-md-6'>
-                                <div className='note-editor-container'>
-                                <h2 className='add-note-title'>Add Note</h2><br />
-                              
-                                <form>
-                                    <div>
-                                        <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}} className="form-control mt-4" placeholder="Note Title" />
-                                    </div>
-                                    <div>
-                                        <input type="text" onChange={(e)=>{setContent(e.target.value)}} value={content} className="form-control mt-4" placeholder="Note Description" />
-                                    </div>
-                                    <div className='addnote-btn'>
-                                        <button type='button' onClick={addNote} className='btn btn-primary mt-4'>Add Note</button>
-                                    </div>
-                                </form>
-                                </div>
-                                
+                    <div className='note-editor-container'>
+                        <h2 className='add-note-title'>Add Note</h2><br />
+
+                        <form>
+                            <div>
+                                <input type="text" value={title} onChange={(e) => { setTitle(e.target.value) }} className="form-control mt-4" placeholder="Note Title" />
+                            </div>
+                            <div>
+                                <input type="text" onChange={(e) => { setContent(e.target.value) }} value={content} className="form-control mt-4" placeholder="Note Description" />
+                            </div>
+                            <div className='addnote-btn'>
+                                <button type='button' onClick={addNote} className='btn btn-primary mt-4'>Add Note</button>
+                            </div>
+                        </form>
+                    </div>
+
                 </div>
             </div>
 
